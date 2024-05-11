7555b5eb6ea9c680e0e44bfb8534db518ded8790
Oskar Hosken <ohos0003@student.monash.edu>
2024-05-11 13:07:38 +1000

    Messing around with launch screen and icon

---
+++ GolfApp/GolfApp/CircleAnnotationView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleAnnotationView.swift
+//  GolfApp
+//
+//  Created by Oskar Hosken on 11/5/2024.
+//
+
+import UIKit
+import MapKit
+
+class CircleAnnotationView: MKAnnotationView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
