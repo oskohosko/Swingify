dd64f82e9e9241104647acbd3a1053ef6c121807
Oskar Hosken <ohos0003@student.monash.edu>
2024-05-24 13:58:32 +1000

    Shortcut to home course done.

--- GolfApp/GolfApp/HomeViewController.swift
+++ GolfApp/GolfApp/HomeViewController.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, DatabaseListener, ProfileUpdateDelegate {
+    
+    var listenerType = ListenerType.profile
+    weak var databaseController: DatabaseProtocol?
+    // This is the user
+    var currentProfile: Profile?
+    
+    // Flag for favourites view
+    var favouriteSelected = false
+    
+    // Outlets for our views
     @IBOutlet weak var courseView: UIView!
     
     @IBOutlet weak var clubsView: UIView!
@@ -21,14 +30,24 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak var infoView: UIView!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        databaseController = appDelegate?.databaseController
+        
+        // Fetching profile
+        let profiles = databaseController?.fetchProfile()
+        if profiles!.count > 0 {
+            currentProfile = profiles![0]
+            updateTextFields(profile: currentProfile!)
+            print(profiles![0].name)
+        }
+        
+        
         
         // Rounding views on the dashboard
         courseView.layer.cornerRadius = 15
@@ -66,16 +85,30 @@ class HomeViewController: UIViewController {
         
         let tapInfoView = UITapGestureRecognizer(target: self, action: #selector(infoViewTapped(_:)))
         infoView.addGestureRecognizer(tapInfoView)
-        
-        
-        
-        
-        
-        
+    }
+    
+    func didUpdateProfile() {
+        let profiles = databaseController?.fetchProfile()
+        currentProfile = profiles![0]
+        updateTextFields(profile: currentProfile!)
+    }
+    
+    func onClubChange(change: DatabaseChange, clubs: [Club]) {
+        // Nothing
+    }
+    
+    func onProfileChange(change: DatabaseChange, profiles: [Profile]) {
+        currentProfile = profiles[0]
+        updateTextFields(profile: currentProfile!)
+    }
+    
+    func onFavCoursesChange(change: DatabaseChange, faveCourses: [FavCourse]) {
+        // Nothing
     }
     
     
     @objc func courseViewTapped(_ sender: UITapGestureRecognizer) {
+        favouriteSelected = false
         performSegue(withIdentifier: "viewCoursesSegue", sender: self)
     }
     
@@ -85,6 +118,10 @@ class HomeViewController: UIViewController {
     
     @objc func homeCourseViewTapped(_ sender: UITapGestureRecognizer) {
         // Do nothing just yet
+        if let currentProfile {
+            performSegue(withIdentifier: "homeCourseSegue", sender: self)
+        }
+        
     }
     
     @objc func profileViewTapped(_ sender: UITapGestureRecognizer) {
@@ -93,20 +130,44 @@ class HomeViewController: UIViewController {
     
     @objc func favouritesViewTapped(_ sender: UITapGestureRecognizer) {
         // Nothing yet
+        favouriteSelected = true
+        performSegue(withIdentifier: "viewCoursesSegue", sender: self)
     }
     
     @objc func infoViewTapped(_ sender: UITapGestureRecognizer) {
         // Nothing yet
     }
+    
+    func updateTextFields(profile: Profile) {
+        // This function when called will update the text fields to show the profile
+        navigationItem.title = "Hey, \(profile.name)!"
+    }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if let profileVC = segue.destination as? ProfileViewController {
+            profileVC.delegate = self
+        }
+        
+        if segue.identifier == "viewCoursesSegue" {
+            let destinationVC = segue.destination as! CoursesTableViewController
+            if favouriteSelected {
+                destinationVC.updateStarToggleButton()
+                destinationVC.toggleFavourites(self)
+            }
+        }
+        
+        if segue.identifier == "homeCourseSegue" {
+            let destinationVC = segue.destination as! HolesTableViewController
+            if let currentProfile {
+                destinationVC.selectedCourseID = Int(currentProfile.courseID)
+            }
+        }
     }
-    */
 
 }
---
+++ GolfApp/GolfApp/ProfileUpdateDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileUpdateDelegate.swift
+//  Swingify
+//
+//  Created by Oskar Hosken on 24/5/2024.
+//
+
+import Foundation
+
+protocol ProfileUpdateDelegate: NSObject {
+    func didUpdateProfile()
+}
